13e0594710a44924d2622b87a0bbf95adb7bc1a9
Florian WILLI <66095313+Florexus@users.noreply.github.com>
2024-11-17 06:08:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,14 @@
-//create web server
+// Create web server
+// Create a web server that listens to the port 3000 and serves the comments.html file
 var http = require('http');
 var fs = require('fs');
 
-//create server
-var server = http.createServer(function(req, res) {
-  console.log('request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(res);
-});
-
-//listen to port 3000
-server.listen(3000, function() {
+http.createServer(function(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.readFile('comments.html', function(err, contents) {
+    response.write(contents);
+    response.end();
+  });
+}).listen(3000, function() {
   console.log('Server is listening on port 3000');
 });
